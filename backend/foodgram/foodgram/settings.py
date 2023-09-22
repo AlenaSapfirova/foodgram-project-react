94dd7e56d96411a8b68fb4c079514ac38963d430
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-09-22 15:00:55 +0300

    fixed 403

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -29,14 +29,15 @@ DEBUG = True
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 SECRET_KEY = 'django-insecure-htbb^sbmx+z0t-i63!=))osf^t^$0y#w29s+2(r5%7n__pn43#'
-ALLOWED_HOSTS = [
-    '158.160.77.21',
-    'localhost',
-    '127.0.0.1',
-    'alenasap.ddns.net',
-]
-# ALLOWED_HOSTS = ['*']
-# CSRF_TRUSTED_ORIGINS = ['https://*.alenasap.ddns.net']
+# ALLOWED_HOSTS = [
+#     '158.160.77.21',
+#     'localhost',
+#     '127.0.0.1',
+#     'alenasap.ddns.net',
+# ]
+
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.alenasap.ddns.net']
 
 
 # Application definition
