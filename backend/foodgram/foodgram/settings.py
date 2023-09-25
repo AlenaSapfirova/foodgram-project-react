e1d2656f4b283624bb9253174f3d3544a297d74a
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-09-25 16:05:25 +0300

    debug-false_1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-
-DEBUG = os.getenv('DEBUG', '')
+DEBUG = False
+# DEBUG = os.getenv('DEBUG', '')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
