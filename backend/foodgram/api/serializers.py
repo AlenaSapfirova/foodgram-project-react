31d1eb011ef5d5eb930407121656f5a2c16100ae
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-09-26 02:49:19 +0300

    TAG_1

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -292,7 +292,7 @@ class CreateUpdateRecipesSerializer(serializers.ModelSerializer):
                 'Ошибка: нет тэгов в рецепте'
             )
         for tag in tags:
-            if tag not in Tag.objects.all():
+            if tag not in Tag.objects.get(id=tag).exists():
                 raise serializers.ValidationError(
                     'Такого тэга не существует.'
                 )
