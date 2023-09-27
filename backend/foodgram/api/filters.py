8da195a8616a0f8fc43ce9d2a1f6387e44f02868
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-09-27 16:27:26 +0300

    favorite_7

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -10,15 +10,16 @@ class CustomFilters(filters.FilterSet):
                                              to_field_name='slug',
                                              queryset=Tag.objects.all())
     author = filters.NumberFilter(field_name='author')
-    is_favorited = filters.BooleanFilter(field_name='is_favorited')
-    #  method='get_is_favorited')
+    is_favorited = filters.BooleanFilter(field_name='is_favorited',
+                                         method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        field_name='is_in_shopping_cart')
-    #     method='get_is_in_shopping_cart'
-    # )
+        field_name='is_in_shopping_cart',
+        method='get_is_in_shopping_cart'
+    )
 
-    # def get_is_favorited(self, queryset, name, value):
-    #     user = self.request.user
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        return queryset.filter(recipes_favorite_recipes__user=user)
     #     if user.is_authenticated and value is True
     # and name == 'is_favorited':
     #         return queryset.filter(recipes_favorite_recipes__user=user)
